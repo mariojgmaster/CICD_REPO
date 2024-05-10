922cabfe12a62f7596f548648fb693c0d4d31172
mariojgmaster <mariojgmaster@yahoo.com.br>
2024-05-10 18:33:50 -0300

    first

---
+++ index.js
@@ -0,0 +1,13 @@
+/**
+    * @description      : 
+    * @author           : mario
+    * @group            : 
+    * @created          : 10/05/2024 - 18:33:39
+    * 
+    * MODIFICATION LOG
+    * - Version         : 1.0.0
+    * - Date            : 10/05/2024
+    * - Author          : mario
+    * - Modification    : 
+**/
+console.log('Test...');
